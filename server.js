4cd9e561b4ed72674e3cf8db6064b8dd0af1aeee
Aaron Ray <aaray619@gmail.com>
2020-10-25 17:38:45 -0500

    added functionality to the delete button

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function (req, res) {
     fs.readFile("./db/db.json", "utf8", function (err, data) {
         if (err) throw err;
-        console.log(data);
+        // console.log(data);
         const newData = JSON.parse(data);
         res.json(newData);
     })
@@ -36,16 +36,46 @@ app.post("/api/notes", function (req, res) {
     // console.log(req.body);
     fs.readFile("./db/db.json", "utf8", function (err, data) {
         if (err) throw err;
-        console.log(data);
-        const studentList = JSON.parse(data);
-        studentList.push(req.body);
-        console.log(studentList);
-        fs.writeFile("./db/db.json", JSON.stringify(studentList), function (err) {
+        // console.log(data);
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        req.body.id = uniqueId;
+       let noteList = JSON.parse(data);
+        noteList.push(req.body);
+        fs.writeFile("./db/db.json", JSON.stringify(noteList), function (err) {
             if (err) throw err;
             console.log("added");
             res.redirect("/notes");
         });
     });
 })
+// app.get("/api/notes/:id", function(req, res) {
+//     res.json(req.params.id);
+// });
+app.delete("/api/notes/:id", function (req, res) {   
+    res.send('Got a DELETE request at /user')
+    console.log("test2");    
+    //splice or slice
+    let noteId = req.params.id;
+    console.log(noteId)
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        let noteList = JSON.parse(data)
+        console.log(noteList)
+        noteList.forEach((item, i)=> {
+            if(item.id === noteId){
+                noteList.splice(i, 1);
+            }
+        })
+        console.log(noteList);
+        fs.writeFile("./db/db.json", JSON.stringify(noteList),function (err) {
+            if (err) throw err;
+            console.log("deleted");
+    
+        });
+    
+    })
+    
+});
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
